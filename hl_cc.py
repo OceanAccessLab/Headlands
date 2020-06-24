ce202bdbd06fa586e3a33a38a846cfd7d0150887
Giulia Bronzi <giuliabronzi@Giulias-MBP.attlocal.net>
2020-06-23 20:36:15 -0400

    module updates

--- hl_cc.py
+++ hl_cc.py
@@ -34,73 +34,28 @@ df_all = hl_modules.readrpf(fileList)
 df_all = hl_modules.removeDuplicates(df_all)
 
 headerInfo = hl_modules.extractHeaders(fileList)
-
+ 
 
 
 # monthly average
 df_monthly = df_all.resample('M').mean()
-df_monthly.plot()
-plt.title("Monthly Average")
-plt.show()
+# df_monthly.plot()
+# plt.title("Monthly Average")
+# plt.show()
 
-df_monthly.to_csv('comfort_cove_thermograph_1989-2017_monthly.csv')
-
-
-# #June-July only:
-# df_summer = df_monthly[(df_monthly.index.month>=6) & (df_monthly.index.month<=7)]
-# df_summer = df_summer.resample('As').mean()
-# df_summer.index = df_summer.index.year
-# df_summer.to_csv('comfort_cove_thermograph_1989-2017_June-July.csv')
+# df_monthly.to_csv('comfort_cove_thermograph_1989-2017_monthly.csv')
 
 
-# ## ---- plot summer data in anomalies ---- ##
-# anom = (df_summer - df_summer.mean()) / df_summer.std()
-# df1 = anom[anom<0]
-# df2 = anom[anom>0]
-# fig = plt.figure(4)
-# fig.clf()
-# width = .9
-# p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='steelblue')
-# p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='indianred')
-# plt.ylabel('Standardized Anomaly')
-# #plt.xlabel('Year')
-# plt.title('Comfort Cove temperature (June-July)')
-# plt.grid()
-# fig.set_size_inches(w=15,h=9)
-# fig_name = 'Comfort_Cove_anomalies.png'
-# #plt.annotate('data source: NCDC/NOAA', xy=(.75, .01), xycoords='figure fraction', annotation_clip=False, FontSize=12)
-# fig.savefig(fig_name, dpi=300)
-# os.system('convert -trim ' + fig_name + ' ' + fig_name)
+#plot comparing annual curve to average curve from 1989-2018
+hl_modules.plotAnnualCurve(df_all, 2018)
 
 
-## ---- annual curve ---- ##
-#originally looking at 2018 but wasnt in file, using 2011 instead
-df_all['woy'] = df_all.index.weekofyear
-weekly_clim = df_all.groupby('woy').mean()
-weekly_std = df_all.groupby('woy').std()
-df_2011 = df_all[df_all.index.year>=2011]
-weekly_2011 = df_2011.groupby('woy').mean()
+hl_modules.plotAnomalies(df_monthly, 6,8)
 
-ax = weekly_clim.plot(linewidth=3, legend=None)
-weekly_2011.plot(ax=ax, linewidth=3)
-plt.fill_between(weekly_clim.index, 
-                  np.squeeze(weekly_clim.values+weekly_std.values),
-                  np.squeeze(weekly_clim.values-weekly_std.values), 
-                  facecolor='steelblue', 
-                  interpolate=True , 
-                  alpha=.3)
-plt.ylabel(r'T ($^{\circ}C$)')
-plt.xlabel('Week of the year')
-plt.title('Comfort Cove temperature')
-plt.xlim([0,53])
-plt.ylim([-2,18])
-plt.grid()
-plt.legend(['1989-2018 average', '2011']) #not correct years
-fig = ax.get_figure()
-fig.set_size_inches(w=12,h=8)
-fig_name = 'Comfort_Cove_T.png'
-fig.savefig(fig_name, dpi=200)
-os.system('convert -trim ' + fig_name + ' ' + fig_name)
+
+
+
+
 
 
 
