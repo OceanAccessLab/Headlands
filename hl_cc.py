4cfd0640438a44e73f1aff9dc0bc2d3cebf6995a
Giulia Bronzi <giuliabronzi@Giulias-MBP.attlocal.net>
2020-08-11 16:46:42 -0400

    upwell module code

--- hl_cc.py
+++ hl_cc.py
@@ -1,6 +1,5 @@
 '''
-data in : /home/cyrf0006/data/Headlands/
-process in : /home/cyrf0006/AZMP/Headlands
+
 
 '''
 
@@ -26,43 +25,26 @@ df_all = hl_modules.removeDuplicates(df_all)
 headerInfo = hl_modules.extractHeaders(fileList)
  
 
-
-# monthly average
-df_monthly = df_all.resample('M').mean()
-# df_monthly.plot()
-# plt.title("Monthly Average")
-# plt.show()
-
-#df_monthly.to_csv('comfort_cove_thermograph_1989-2017_monthly.csv')
-
-
-
 # plot comparing annual curve to average curve from 1989-2018
 #hl_modules.plotAnnualCurve(df_all, 2007, "ComfortCove")
 
 
 # plots monthly anomalies for June-July
-#hl_modules.plotAnomalies(df_monthly, 6,7, "ComfortCove")
-
+#hl_modules.plotAnomalies(df_all, 6,7, "ComfortCove")
 
 
 # time series plots for summer months 
-#hl_modules.plotTSMonth(df_monthly, 6, 8, "ComfortCove")
-#hl_modules.plotTSMonth(df_monthly, 7, 7, "ComfortCove")
+#hl_modules.plotMonthAverage(df_all, 6, 8, "ComfortCove")
+#hl_modules.plotMonthAverage(df_all, 7, 7, "ComfortCove")
 
 
 #time series for everyday out of a certain month
-#hl_modules.plotTSDay(df_all, 1997, 6, "comfort cove")
+#hl_modules.plotDailyTS(df_all, 1997, 6, "comfort cove")
 
 
 #creates an error when you run both at the same time 
-#hl_modules.findClimatology(df_all, 2007, "Comfort Cove", 'woy')
+#hl_modules.plotClimatology(df_all, 2007, "Comfort Cove", 'woy')
 #hl_modules.plotClimatology(df_all, 2007, "Comfort Cove")
-
-
-upwellDates = hl_modules.plotUpwells(df_all, 2007, "Comfort Cove")
-
-print(hl_modules.upwellTable(upwellDates))
 
 
 
@@ -70,6 +52,26 @@ print(hl_modules.upwellTable(upwellDates))
 
 
 
+upwellDates = hl_modules.getUpwellDates(df_all, 2017, "Comfort Cove")
+print(hl_modules.upwellDF(upwellDates))
+
+
+#'Lumsden/5m', 'Lumsden/15m', 'Melrose/5m', 'Melrose/15m',, 
+
+siteNames = ['ArnoldsCove', 'BristolsHope', 'ComfortCove', 'Hampden', 
+         'OldBonaventure', 'StockCove', 'UpperGullies', 'Winterton']
+
+for site in siteNames:
+    fileList = hl_modules.getFileList(site)
+    df_all = hl_modules.readrpf(fileList)
+    df_all = hl_modules.removeDuplicates(df_all)
+    hl_modules.upwellsToCSV(df_all, site)
+
+
+
+
 # netcdf
 #print(hl_modules.convertNetCDF(df_all, headerInfo, "ComfortCove"))
+
+
 
